8c19f1910792dc9823c152e5e79de2eec956a1de
bin <bin@bin-pc>
2019-09-04 16:31:09 +0900

    초기설정 commit

---
+++ develop-demo-api/src/main/java/demo/api/DevelopDemoApiApplication.java
@@ -0,0 +1,13 @@
+package demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopDemoApiApplication.class, args);
+	}
+
+}
